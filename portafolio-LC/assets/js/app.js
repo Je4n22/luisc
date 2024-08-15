6c073b5ac7f79e6921f850bded75f8cc4f56af2d
Je4n22 <luis.cornejo.u@hotmail.com>
2024-08-14 22:53:34 -0400

    primer commit portafolio

---
+++ portafolio-LC/assets/js/app.js
@@ -0,0 +1,20 @@
+function funcionocultar() {
+    var x = document.getElementById("div_mostrar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function funcionocultar2() {
+    var x = document.getElementById("div_mostrar2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function funcionfecha(){
+let now = new Date();
+alert( now );
+}
